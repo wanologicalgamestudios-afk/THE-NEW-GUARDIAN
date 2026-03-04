62b25d0c649e9876ad490d200db953762c43116c
amaannaeem79 <91053598+amaannaeem79@users.noreply.github.com>
2026-03-04 09:29:25 +0500

    working on environment

--- Assets/Scripts/GamePlay/GamePlay.cs
+++ Assets/Scripts/GamePlay/GamePlay.cs
@@ -3,19 +3,22 @@ using UnityEngine;
 public class GamePlay : MonoBehaviour
 {
     [SerializeField] GameObject mainCharacter;
-    [SerializeField] CharacterController mainCharacterController;
+    [SerializeField] GameObject environment;
+    [SerializeField] CharacterControllerCustom mainCharacterController;
 
 
     private void StartGame() 
     {
         Debug.Log("StartGame");
         mainCharacter.SetActive(true);
+        environment.SetActive(true);
         mainCharacterController.SetCharacterForNewGame();
     }
 
     public void ExitGame() 
     {
         mainCharacter.SetActive(false);
+        environment.SetActive(false);
     }
     
     public void StartNewGame() 
