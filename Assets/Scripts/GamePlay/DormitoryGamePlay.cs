0f68898da7aef5e6df3eaa2e14f19e9d35f3f067
amaannaeem79 <91053598+amaannaeem79@users.noreply.github.com>
2026-03-09 09:20:11 +0500

    was working on camera panning in scene two.

--- Assets/Scripts/GamePlay/DormitoryGamePlay.cs
+++ Assets/Scripts/GamePlay/DormitoryGamePlay.cs
@@ -1,15 +1,26 @@
+using TMPro;
 using UnityEngine;
 
 public class DormitoryGamePlay : MonoBehaviour
 {
+    [SerializeField] Camera mainCamera;
+    [SerializeField] float cameraPanTime;
+    [SerializeField] float cameraSizeTo;
+    [SerializeField] Vector3 cameraPositionTo;
+
     [SerializeField] Vector3 characterPositionAtSceneBegin;
     [SerializeField] Vector3 characterPositionAtGameStart;
     [SerializeField] CharacterControllerCustom characterController;
+    [SerializeField] float timeToBeginScene;
     [SerializeField] float timeToStartYawnAnimation;
     [SerializeField] float timeToStartGame;
     [SerializeField] Obstacle samBed;
 
     private GamePlayUI gamePlayUI;
+
+    private float cameraOrignalSize;
+    private Vector3 cameraOrignalPosition;
+
 
     private void Start()
     {
@@ -18,20 +29,63 @@ public class DormitoryGamePlay : MonoBehaviour
 
     private void BeginScene() 
     {
-        Debug.Log("StartGame");
         characterController.transform.position = characterPositionAtSceneBegin;
         characterController.AnimatorController.PlaySittingAnimation();
-        Invoke(nameof(PlayYawnAnimation), timeToStartYawnAnimation);
+        Invoke(nameof(StartYawnAnimation), timeToStartYawnAnimation);
+        //Invoke(nameof(PanTheCamera), timeToBeginScene); 
+    }
+    private void PanTheCamera() 
+    {
+        cameraOrignalSize = mainCamera.orthographicSize;
+        cameraOrignalPosition = mainCamera.transform.position;
+        ZoomTheCamera(mainCamera.gameObject,cameraOrignalSize, cameraSizeTo, cameraPanTime);
+     
+      //  MovetheCameraToPosition();
+    }
+    private void ZoomTheCamera(GameObject _gameObject, float _from, float _to, float _time) 
+    {
+        iTween.ValueTo(_gameObject, iTween.Hash(
+        "from", _from,
+        "to", _to,
+        "time", _time,
+        "onupdate", nameof(ZoomTheCameraUpdateMethod),
+        "onupdatetarget", this.gameObject,
+        "oncomplete", nameof(ZoomTheCameraCompleteMethod),
+        "oncompletetarget", this.gameObject
+         ));
     }
 
-    private void PlayYawnAnimation()
-    { 
-         characterController.AnimatorController.PlayYawnAnimation(CallbackOnYawnAnimationEnd);
+    private void ZoomTheCameraUpdateMethod(float newValue) 
+    {
+        mainCamera.orthographicSize = newValue;
     }
+
+    private void ZoomTheCameraCompleteMethod()
+    { 
+       
+    }
+
+
+    private void MovetheCameraToPosition(GameObject _gameObject, Vector3 _toPosition, float _moveToTime)
+    {
+        iTween.MoveTo(_gameObject, iTween.Hash(
+            "position", _toPosition,
+            "time", _moveToTime,
+            "oncomplete", "MoveCompleted",
+            "oncompletetarget", gameObject
+            ));
+    }
+
+
 
     private void CallbackOnYawnAnimationEnd()
     {
         Invoke(nameof(StartGameplay), timeToStartGame);
+    }
+
+    private void StartYawnAnimation() 
+    {
+        characterController.AnimatorController.PlayYawnAnimation(CallbackOnYawnAnimationEnd);
     }
 
     private void StartGameplay() 
